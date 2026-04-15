6446a210f324614bceaebb7c36bd90f5c5385f30
KAPACOT <42932243+KAPACOT@users.noreply.github.com>
2026-04-15 16:42:35 +0000

    none

--- smart-dashboard/src/modules/notes/notesDummyData.js
+++
@@ -1,4 +0,0 @@
-export const notesDummy = [
-  { id: 1, content: "Купить продукты", date: new Date().toISOString(), points: 2 },
-  { id: 2, content: "Идея для проекта", date: new Date().toISOString(), points: 1 }
-];
